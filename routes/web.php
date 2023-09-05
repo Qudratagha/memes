d1cb0a51a690d13915f3629e297fcb26cef053fb
Asfandyarkhanafridi <masfandy9@gmail.com>
2023-09-05 19:50:13 +0500

    front end added

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('/meme', App\Http\Controllers\MemeController::class);
